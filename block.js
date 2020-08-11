ffcfd91c7d1018db8fda3911077bf7e1d1ff913a
ArnavGoelHitman <68644498+ArnavGoelHitman@users.noreply.github.com>
2020-08-11 20:40:41 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,29 @@
+class Block {
+    constructor(x, y, width, height){
+      var options = {
+        isStatic:false,
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width=width;
+      this.height=height;
+      World.add(world,this.body);
+
+     
+    }
+    display(){
+      var pos =this.body.position;
+      
+      push();
+      fill ("red");
+      translate(this.body.position.x, this.body.position.y);
+      rotate (this.body.angle)
+      rectMode (CENTER);
+      rect(0,0, this.width, this.height);
+     
+      pop();
+      
+   
+    }
+  
+  };
+  
